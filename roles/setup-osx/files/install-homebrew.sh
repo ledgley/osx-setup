12527bdfca32058226ceded6b18f3a139f49482f
Luke Edgley <lukeedgley@gmail.com>
2018-11-15 13:54:13 +1100

    Update install-homebrew.sh

--- roles/setup-osx/files/install-homebrew.sh
+++ roles/setup-osx/files/install-homebrew.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-yes | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
